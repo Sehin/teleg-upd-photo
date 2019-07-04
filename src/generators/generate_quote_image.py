eb2561149d87290f493228f86223e8b0e0eedd5c
Глотов Дмитрий <glotovdi@gmail.com>
2019-07-04 16:40:43 +0300

    logs

--- src/generators/generate_quote_image.py
+++ src/generators/generate_quote_image.py
@@ -38,6 +38,7 @@ def get_shiba():
     photo_url = re.findall('.*"(.*)"', r.text)[0]
     # photo_file = urllib.URLopener()
     # photo_file.retrieve(photo_url, "shiba.jpg")
+    print(photo_url)
     urllib.request.urlretrieve(photo_url, "shiba.jpg")
 
 def generate_image_with_text():
